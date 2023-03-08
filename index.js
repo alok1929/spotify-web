3ad7ba691be1f51e1250992259018ba2d9c14c85
Alok Hegde <alokhegde221@gmail.com>
2023-03-09 02:16:56 +0530

    didnt get access token

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-const express=require('express')
+const SpotifyWebApi = require('spotify-web-api-node');
 
+const spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(access_token);
 
-client_id="8497fd1eb5bb4dbaa481fb40dac207d8"
-client_secret="465a2c4dd6a6406d8ebc5d653686cd5c"
-var redirect_uri = 'http://localhost:8888/callback';
-
-var app = express();
-
-const queryString = require("querystring");
-const axios = require("axios");
-
-app.get("/account", async (req, res) => {
-  const spotifyResponse = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      queryString.stringify({
-        grant_type: "authorization_code",
-        code: req.query.code,
-        redirect_uri: redirect_uri,
-      }),
-      {
-        headers: {
-          Authorization: "Basic " + client_secret,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-  
-  console.log(spotifyResponse.data);
-})
-const data = axios.get(
-    "https://api.spotify.com/v1/me/top/tracks?limit=50",
-    {
-      headers: {
-        Authorization: "Bearer " + spotifyResponse,
-      },
-    }
-  );
-
-  console.log(data)
+(async () => {
+  const me = await spotifyApi.getMe();
+  console.log(me);
+})().catch(e => {
+  console.error(e);
+});
