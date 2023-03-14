fab80948104bbf11b437aac3d40bd7b3bf373ab4
Alok Hegde <alokhegde221@gmail.com>
2023-03-15 01:48:09 +0530

    didnt work, again, but made song play on spotify

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-const SpotifyWebApi = require('spotify-web-api-node');
+const CLIENT_ID = '8497fd1eb5bb4dbaa481fb40dac207d8';
+const REDIRECT_URI = 'http://localhost:3000/callback';
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
-// Replace these values with your own client ID and client secret
-const clientId = '8497fd1eb5bb4dbaa481fb40dac207d8';
-const clientSecret = '849f050086a14cfc875261ba7804b079';
-
-// Create a new instance of the Spotify API client
-const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret
+const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>', {
+  url: 'http://localhost'
 });
 
-console.log("credential successful")
-// Authenticate with the Spotify API
-spotifyApi.clientCredentialsGrant().then((data) => {
-  spotifyApi.setAccessToken(data.body.access_token);
+// Simulate the window and document objects
+global.window = dom.window;
+global.document = dom.window.document;
 
-  spotifyApi.getPlaylistTracks('37i9dQZF1E8TQ0azKAsfW0')
-    .then(data => {
-      // Extract the list of artist IDs from each track
-      const artistIds = data.body.items.map(item => item.track.artists.map(artist => artist.id)).flat();
+// Initialize the Spotify Web Playback SDK
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const player = new Spotify.Player({
+    name: 'My Web Playback SDK Player',
+    getOAuthToken: callback => {
+      // Use the Authorization Code Flow to authenticate the app
+      if (window.location.hash.includes('access_token')) {
+        // If the access token is already in the URL hash, use it
+        const params = new URLSearchParams(window.location.hash.substring(1));
+        const accessToken = params.get('access_token');
+        callback(accessToken);
+      } else {
+        // If the access token is not in the URL hash, redirect the user to the authorization page
+        const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=user-read-playback-state,user-modify-playback-state`;
+        window.location.href = authUrl;
+      }
+    }
+  });
 
-      // Use the getArtist method to retrieve the genres for each artist ID
-      const promises = artistIds.map(artistId => spotifyApi.getArtist(artistId));
-      return Promise.all(promises);
-    })
-    .then(data => {
-      // Extract the genres from the artist objects
-      const genres = data.map(artist => artist.body.genres).flat();
+  // Connect to the player
+  player.connect();
 
-      // Remove duplicates from the list of genres
-      const uniqueGenres = [...new Set(genres)];
-
-      // Log the list of unique genres to the console
-      console.log(uniqueGenres);
-    })
-    .catch(error => console.error(error));
-})
-.catch(error => console.error(error));
-
-//const delay = ms => new Promise(res => setTimeout(res, ms));
-
-// This function will make a request with a delay of 1 second between each request
-//async function makeRequests() {
-  //for (let i = 0; i < 10; i++) {
-    //await spotifyApi.getArtist(artistIds[i]);
-    //await delay(1000); // wait for 1 second before making the next request
-  //}
-//}
+  player.addListener('ready', ({ device_id }) => {
+    // Start playing a playlist
+    fetch(`https://api.spotify.com/v1/playlists/YOUR_PLAYLIST_ID/tracks`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        uris: ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M', 'spotify:track:6u7jPi22kF8CTQ3rb9DHE7'] // replace with your desired track URIs
+      })
+    }).then(() => {
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      });
+    });
+  });
+};
